d706c2f7b23d144d9926f063fb1d297f3b0180e6
Javier Mejia <javier.mejia@javiers-mbp.home>
2020-05-03 23:49:02 -0700

    Fixed Public Subnets

--- variables.tf
+++ variables.tf
@@ -1,21 +1,31 @@
-variable "aws_region" {
-    description = "EC2 Region for the VPC"
-    default = "us-west-1"
+variable "region" {
+    default     = "us-west-1"
+    description = "AWS Region"
 }
 
 variable "vpc_cidr" {
+    default     = "10.0.0.0/16"
     description = "VPC CIDR"
-    default = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
-    description = "Public Subnet"
-    default = "10.0.1.0/24"
+variable "public_subnet_1_cidr" {
+    description = "Public Subnet 1 CIDR"
+    default     = "10.0.1.0/24"
 }
 
-variable "private_subnet_cidr" {
-    description = "Private Subnet"
-    default = "10.0.2.0/24"
+variable "public_subnet_2_cidr" {
+    description = "Public Subnet 2 CIDR"
+    default     = "10.0.3.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+    description = "Private Subnet 1 CIDR"
+    default     = "10.0.2.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+    description = "Private Subnet 2 CIDR"
+    default     = "10.0.4.0/24"
 }
 
 variable "ingress_ports" {
@@ -26,5 +36,5 @@ variable "ingress_ports" {
 
 variable "Name" {
     description = "Name"
-    default     = "Non-Default"
+    default     = "BlackMamba"
 }
