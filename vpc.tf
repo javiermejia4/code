119933634b0017ca4021881889a91365a9f28292
Javier Mejia <jmejia@ellimemae.com>
2021-01-26 23:35:54 -0800

    Added correct allowed IP address

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,6 @@ resource "aws_vpc" "production-vpc" {
     Name                 = "BlackMamba-VPC"
   }
 }
-
 ## Public Subnet
 resource "aws_subnet" "public-subnet-1" {
   cidr_block  = var.public_subnet_1_cidr
@@ -16,7 +15,6 @@ resource "aws_subnet" "public-subnet-1" {
     Name      = "BlackMamba-Public-Subnet-1"
   }
 }
-
 ## Private Subnet
 resource "aws_subnet" "private-subnet-1" {
   cidr_block  = var.private_subnet_1_cidr
@@ -26,7 +24,6 @@ resource "aws_subnet" "private-subnet-1" {
     Name      = "BlackMamba-Private-Subnet-1"
   }
 }
-
 ## Private Subnet 2
 resource "aws_subnet" "private-subnet-2" {
   cidr_block  = var.private_subnet_2_cidr
@@ -36,7 +33,6 @@ resource "aws_subnet" "private-subnet-2" {
     Name      = "BlackMamba-Private-Subnet-2"
   }
 }
-
 ## Private Subnet
 resource "aws_subnet" "private-subnet-3" {
   cidr_block  = var.private_subnet_3_cidr
@@ -46,7 +42,6 @@ resource "aws_subnet" "private-subnet-3" {
     Name      = "BlackMamba-Private-Subnet-3"
   }
 }
-
 ## Public Route Table
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.production-vpc.id
@@ -55,7 +50,6 @@ resource "aws_route_table" "public-route-table" {
     Name = "BlackMamba-Public-Route-Table"
   }
 }
-
 ## Private Route Table
 resource "aws_route_table" "private-route-table" {
   vpc_id = aws_vpc.production-vpc.id
@@ -65,7 +59,6 @@ resource "aws_route_table" "private-route-table" {
 
   }
 }
-
 ## Route Table Associations
 resource "aws_route_table_association" "public-subnet-1-association" {
   route_table_id = aws_route_table.public-route-table.id
@@ -76,7 +69,6 @@ resource "aws_route_table_association" "private-subnet-1-association" {
   route_table_id = aws_route_table.private-route-table.id
   subnet_id      = aws_subnet.private-subnet-1.id
 }
-
 ## IGW
 resource "aws_internet_gateway" "production-igw" {
   vpc_id = aws_vpc.production-vpc.id
@@ -91,7 +83,6 @@ resource "aws_route" "public-internet-gateway-route" {
   gateway_id             = aws_internet_gateway.production-igw.id
   destination_cidr_block = "0.0.0.0/0"
 }
-
 ## Security Groups
 resource "aws_security_group" "allowed_traffic" {
     description = "Ingress Traffic"
@@ -108,7 +99,7 @@ resource "aws_security_group" "allowed_traffic" {
        from_port   = port.value
        to_port     = port.value
        protocol    = "TCP"
-       cidr_blocks = ["23.243.44.14/32"]
+       cidr_blocks = ["45.50.88.178/32"]
      }
    }
 }
