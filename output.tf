d35404784bb19dfab3fe138aa983d22243b9892c
Javier Mejia <jmejia@ellimemae.com>
2021-03-16 20:34:50 -0700

    added 2 additional outputs

--- output.tf
+++ output.tf
@@ -6,3 +6,11 @@ output "checklist" {
 output "expected" {
   value = aws_security_group.allowed_traffic[*].ingress
 }
+
+output "aws_public-subnet-1" {
+  value = aws_subnet.public-subnet-1.id
+}
+
+output "aws_private-subnet-1" {
+  value = aws_subnet.private-subnet-1.id
+}
