89010ed46994eb5880802c6e361f0b638ea5a88f
javiermejia4 <17503661+javiermejia4@users.noreply.github.com>
2021-05-03 21:33:07 -0700

    Instance count (#36)
    * added count for bastion
    
    * changed a bunch of stuff
    
    * more stuff
    
    * changed index
    
    * fixed index
    
    Co-authored-by: Javier Mejia <jmejia@ellimemae.com>

--- bastion.tf
+++ bastion.tf
@@ -1,20 +1,20 @@
-resource "aws_instance" "blackmamba-bastion-1" {
+resource "aws_instance" "blackmamba-bastion" {
   ami                         = var.linux_ami
   instance_type               = var.intance_type
   key_name                    = var.key_pair_name
   associate_public_ip_address = true
 
-  count = 2
+  count = 1
 
   subnet_id              = aws_subnet.public-subnet-1.id
   vpc_security_group_ids = [aws_security_group.allowed_traffic.id]
 
-  user_data = "sudo apt update && sudo apt upgrade"
+  user_data = "sudo apt update && sudo apt upgrade -y"
 
   tags = merge(
     local.common_tags,
     {
-      Name  = "blackMamba-bastion-${count.index + 1}"
+      Name = "blackMamba-bastion-0${count.index + 1}"
     },
   )
 }
